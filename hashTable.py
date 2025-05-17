10fb4b67c7ea9f08afe4ce9c3b2413046e92c1cb
Ezequiel-Commits <142255412+Ezequiel-Commits@users.noreply.github.com>
2025-05-17 16:05:06 -0400

    TPP challenge + randomizing the hashFunctions

--- hashTable.py
+++ hashTable.py
@@ -1,14 +1,19 @@
 """Implementation of a hash table: similar to a dictionary"""
 import linkedList
 import hashFunction
+import random
 
 
 class HashTable:
-    def __init__(self, nOfBuckets, hashFunction):
+    def __init__(self, nOfBuckets):
         # Create data structure to make look up by value O(1) on average
         self.HashTable = []
         self.nOfBuckets = nOfBuckets
-        self.hashFunction = hashFunction
+        # select a random hashFunction from the hashFunctionLibrary
+        function = hashFunction.hashFunction()
+        hashFunctions = [function.executeFunction1, function.executeFunction2, function.executeFunction3]
+        randomIndex = random.randint(0, 2)
+        self.hashFunction = hashFunctions[randomIndex]
     
     def createTable(self):
         # Create a hash table with nOfBuckets' elements
@@ -34,7 +39,7 @@ class HashTable:
 
     def addToTable(self, value):
         # 1. execute the hashFunction to get the bucket 
-        bucket = self.hashFunction.executeFunction2(value = value, nOfBuckets = self.nOfBuckets) 
+        bucket = self.hashFunction(value = value, nOfBuckets = self.nOfBuckets) 
         print(value, bucket)
         # 2. add an element to the associated bucket using the pre-defined method
         self.addToBucket(value = value, bucket = bucket)
@@ -42,20 +47,28 @@ class HashTable:
     def search(self, value):
         """Search for an element by value in O(1) time"""
         # 1. Get the bucket of the value using the hashFunction
-        bucket = self.hashFunction.executeFunction3(value = value, nOfBuckets = self.nOfBuckets) 
+        bucket = self.hashFunction(value = value, nOfBuckets = self.nOfBuckets) 
         # 2. use the search method of the linked list to go through each node in the bucket
         return self.HashTable[bucket].search(targetValue = value)
 
 hashFunction1 = hashFunction.hashFunction()
 
-myHashTable = HashTable(nOfBuckets = 10, hashFunction = hashFunction1)
+# Create an object of class HashTable
+myHashTable = HashTable(nOfBuckets = 10)
+# Fill the object(currently an empty array) with buckets(empty linkedLists)
 myHashTable.createTable()
+# Add values to the table
 myHashTable.addToTable(19)
 myHashTable.addToTable(22)
 myHashTable.addToTable(9)
 myHashTable.addToTable(12)
 myHashTable.addToTable(23)
 myHashTable.addToTable(5)
+# Example of an error through using different hash functions. 
 ans = myHashTable.search(9)
-# print(ans)
+print(ans)
+ans = myHashTable.search(22)
+print(ans)
+ans = myHashTable.search(19)
+print(ans)
 # myHashTable.__repr__()
